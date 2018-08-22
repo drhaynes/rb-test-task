60cdf5870f8bdac4a1856a73baa2ca86d5a35c28
drhaynes <drhmbp@gmail.com>
2018-08-22 15:09:15 +0100

    Initial commit. Define sample data

---
+++ MartianRobots.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Martial Robots: Red Badger Coding Test
+
+import Foundation
+
+let sampleInput = """
+5 3
+1 1 E
+RFRFRFRF
+
+3 2 N
+FRRFLLFFRRFLL
+
+0 3 W
+LLFFFLFLFL
+"""
+
+let expectedOutput = """
+1 1 E
+3 3 N LOST
+2 3 S
+"""
+
