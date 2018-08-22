a05ba7a8d36447ec3e62628eef24db3de4193ed6
drhaynes <drhmbp@gmail.com>
2018-08-22 15:21:25 +0100

    Add model types and documentation

--- MartianRobots.playground/Contents.swift
+++ MartianRobots.playground/Contents.swift
@@ -2,6 +2,70 @@
 
 import Foundation
 
+/// Struct that represents an object with an integer size width and height
+struct Size {
+
+    /// The width of the area
+    let width: Int32
+
+    /// Height of the area
+    let height: Int32
+}
+
+/// Coordinate struct for representing integer position on a 2d rectangular grid
+struct Coordinate {
+
+    /// X-axis component
+    let x: Int32
+
+    /// Y-axis component
+    let y: Int32
+}
+
+/// Planet representation
+struct Planet {
+
+    /// The size of the plantet's 2d surface area
+    let area: Size
+
+    /// Locations of coordinates known to be on the edge of the area
+    var scentLocations: [Coordinate]
+}
+
+/// Compass orientation ordinal type
+///
+/// - north: Northern direction
+/// - east: Eastern direction
+/// - south: Southern direction
+/// - west: Westerly direction
+enum CompassOrientation {
+    case north
+    case east
+    case south
+    case west
+}
+
+/// Robot representation
+struct Robot {
+
+    /// Current position of the robot
+    var position: Coordinate
+
+    /// Current orientation of the robot
+    var orientation: CompassOrientation
+}
+
+/// Command representation
+///
+/// - turnRight: Command to turn right
+/// - turnLeft: Command to turn left
+/// - moveForwards: Command to move forwards one grid unit
+enum command {
+    case turnRight
+    case turnLeft
+    case moveForwards
+}
+
 let sampleInput = """
 5 3
 1 1 E
@@ -19,4 +83,3 @@ let expectedOutput = """
 3 3 N LOST
 2 3 S
 """
-
